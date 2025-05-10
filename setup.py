f5970d57e51ffd48203dc1344b65a4848a210de5
dangkv <vo.dangkh@gmail.com>
2025-05-10 00:33:59 -0400

    chore: refactor repo structure

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ import os
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
-    long_description = "\n" + fh.read()
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = "\n" + f.read()
 
-VERSION = "0.0.1"
+VERSION = "0.0.3"
 DESCRIPTION = "Simple iRacing API Wrapper"
 LONG_DESCRIPTION = "A Python app to get data from the iRacing API endpoints. Helps you get stats, race results, and more! 🏎️ 🏁"
 
@@ -21,7 +21,7 @@ setup(
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=["requests"],
+    install_requires=["requests", "requests_toolbelt"],
     keywords=[
         "python",
         "api",
